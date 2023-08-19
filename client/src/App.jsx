9b9a6141f249c5a5beb6c748e2cab2821046f976
Aditya-Padwal <aditya.padwal3102@gmail.com>
2023-08-19 23:20:38 +0530

    Fix Account Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import AccountPage from './pages/AccountPage';
 import Layout from './Layout';
 import { UserContextProvider } from './UserContext';
 import axios from 'axios';
@@ -19,6 +20,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account/:subpage?" element={<AccountPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
